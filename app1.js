390014786b22ee421628c99a4d152cc0f26c1ffb
RushikeshPatil8262 <rushikeshpatil8262@gmail.com>
2025-07-19 16:51:23 +0530

    Form Event code

---
+++ app1.js
@@ -0,0 +1,5 @@
+let form= document.querySelector("form");
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); //IMP
+    alert("form submitted"); //To create ALERT shutters.
+})
