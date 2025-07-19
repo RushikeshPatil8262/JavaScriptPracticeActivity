bcd70b746bbf89cb8a70d735f88b36dba7701585
RushikeshPatil8262 <rushikeshpatil8262@gmail.com>
2025-07-19 18:08:49 +0530

    Change event & Input event

--- app1.js
+++ app1.js
@@ -1,5 +1,17 @@
 let form= document.querySelector("form");
 form.addEventListener("submit", function(event) {
     event.preventDefault(); //IMP
-    alert("form submitted"); //To create ALERT shutters.
-})
+   // alert("form submitted"); //To create ALERT shutters.
+});
+
+let user = document.querySelector("#user");
+
+user.addEventListener("change", function() {
+    console.log("change event");
+    console.log("final value = ", this.value);
+});
+
+user.addEventListener("input", function(){
+    console.log("input event");
+    console.log("final value = ",this.value);
+});
