65027bb6d19d06733270e2af7b88d7f54289c6bc
RushikeshPatil8262 <rushikeshpatil8262@gmail.com>
2025-08-24 23:59:13 +0530

    Dog APIs

--- app6.js
+++ app6.js
@@ -1,19 +1,54 @@
-let url = "https://catfact.ninja/fact";
+let btn = document.querySelector("button");
+let url2 = "https://dog.ceo/api/breeds/image/random";
 
-fetch(url)
- .then((res) => {
-    return res.json();
- })
- .then((data1) => {
-    console.log("data1 =",data1.fact);
-    return fetch(url);
- })
- .then((res) => {
-    return res.json();
- })
- .then((data2) => {
-   console.log("data2 =",data2.fact);
- })
- .catch((err) => {
-    console.log("ERROR -", err);
- })
+btn.addEventListener("click", async () => {
+    let link = await getImage ();
+    let img = document.querySelector("#result");
+    img.setAttribute("src", link);
+    console.log(link);
+});
+
+async function getImage() {
+    try {
+        let res = await axios.get(url2);
+       return res.data.message;
+    }
+    catch (e) {
+        console.log("ERROR -", e);
+        return "/";
+    }
+}
+
+// btn.addEventListener("click", async () => {
+//     let fact = await getFacts();
+//     //console.log(fact);
+//     let p = document.querySelector("#result");
+//     p.innerText = fact;
+// });
+
+// let url = "https://catfact.ninja/fact";
+// let url2 = "https://dog.ceo/api/breeds/image/random";
+
+// async function getFacts() {
+//     try {
+//         let res = await axios.get(url);
+//        return res.data.fact;
+//     }
+//     catch (e) {
+//         console.log("ERROR -", e);
+//         return "No fact found";
+//     }
+// }
+
+// async function getFacts() {
+//     try {
+//         let res = await fetch(url);
+//         let data = await res.json();
+//         console.log(data.fact);
+//     }
+//     catch (e) {
+//         console.log("ERROR -", e);
+//     }
+
+//     console.log("Bye Bye");
+// }
