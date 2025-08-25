e315969575c8f03dd370d27caa2644c7d1fca7ba
RushikeshPatil8262 <rushikeshpatil8262@gmail.com>
2025-08-25 22:37:13 +0530

    Updating Query Strings

--- app6.js
+++ app6.js
@@ -1,13 +1,34 @@
-const url = "https://icanhazdadjoke.com/";
+const url = "http://universities.hipolabs.com/search?name=";
+let btn = document.querySelector("button");
 
-async function getJokes() {
-    try {
-        const config = { headers: {Accept : "application/json" } };
-        let res = await axios.get(url, config);
-        console.log(res.data);
+btn.addEventListener("click", async () => {
+    let country = document.querySelector("input").value;
+    console.log(country);
+
+    let colArr = await getColleges(country);
+    show(colArr);
+});
+
+function show(colArr) {
+     let list = document.querySelector("#list");
+     list.innerText = "";
+     for (col of colArr) {
+        console.log(col.name);
+
+    let li = document.createElement("li");
+    li.innerText = col.name;
+    list.appendChild(li);
     }
-    catch(err) {
-        console.log(err);
+}
+
+async function getColleges (country){
+    try {
+        let res = await axios.get(url+country);
+        return res.data;
+    }
+    catch(e) {
+        console.log("error:", e);
+        return [];
     }
 }
 
